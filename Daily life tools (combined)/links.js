42edceb7566a54a63811d1176380bbfb0237de7b
Taaseen <ahnafabrartaaseen09876@gmail.com>
2026-06-16 20:18:31 +0600

    Add files via upload

--- Daily life tools (combined)/links.js
+++ Daily life tools (combined)/links.js
@@ -2,14 +2,18 @@ const bookmarksContainer = document.getElementById("bookmarks");
 let name;
 let url;
 let icon;
+let BookmarkEl;
+let BookmarkId;
 
+let id;
 let bookmarks = JSON.parse(localStorage.getItem("bookmarks")) || [];
 bookmarks.forEach(bookmark => {
 bookmarksContainer.insertAdjacentHTML(
   "beforeend",
-  `<a href="${bookmark.url}" target="_blank" class="link">
-      <img src="${bookmark.icon}" alt="${bookmark.name}" width="40" class="linkImg">
-   </a>`
+    `<a href="${bookmark.url}" target="_blank" class="link" id="${bookmark.id}">
+        <img src="${bookmark.icon}" alt="${bookmark.name}" width="40" class="linkImg">
+        <button class="BookmarkMenu">⋮</button>
+    </a>`
 );});
 const dialog = document.querySelector(".addBookmarkDialog");
 
@@ -30,15 +34,43 @@ document.querySelector(".ok").addEventListener("click", () => {
     const domain = new URL(url).hostname;
     icon = `https://icons.duckduckgo.com/ip3/${domain}.ico`;
   }
+    id = Date.now();
     bookmarksContainer.insertAdjacentHTML(
           "beforeend",
-          `<a href="${url}" target="_blank" class="link">
+
+          `<a href="${url}" target="_blank" class="link" id="${id}">
               <img src="${icon}" alt="${name}" width="40" class="linkImg">
+              <button class="BookmarkMenu">⋮</i></button>
+            
           </a>`
     );
-    bookmarks.push({ name, url, icon });
+    bookmarks.push({ name, url, icon, id });
 
     localStorage.setItem("bookmarks", JSON.stringify(bookmarks));
     dialog.close();
   }
 })
+const BookmarksMenu = document.querySelector(".BookmarksMenu");
+const overlay = document.querySelector(".overlay");
+const Bookmarkdelete = document.querySelector(".Bookmarkdelete");
+bookmarksContainer.addEventListener("click", (e) => {
+  if(e.target.classList.contains("BookmarkMenu")){
+    e.preventDefault();
+    e.stopPropagation();
+    
+    BookmarksMenu.classList.toggle("active");
+    overlay.classList.toggle("active");
+    BookmarkEl = e.target.closest(".link");
+    console.log(BookmarkEl);
+    BookmarkId = Number(BookmarkEl.id);
+  }
+})
+Bookmarkdelete.addEventListener("click", (e) => {
+  if(e.target.classList.contains("Bookmarkdelete")){
+    bookmarks = bookmarks.filter(each => each.id !== BookmarkId);
+    localStorage.setItem("bookmarks", JSON.stringify(bookmarks));
+    BookmarkEl.remove();
+    overlay.classList.remove("active");
+    BookmarksMenu.classList.remove("active");
+  }
+});
